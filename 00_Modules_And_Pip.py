c2e3785b961294f31434752588881a577b1e390c
Bipin Rai <workraibipin@gmail.com>
2024-08-21 04:04:55 +0545

    Modules_And_Pip

---
+++ 00_Modules_And_Pip.py
@@ -0,0 +1,21 @@
+'''
+Modules:
+Modules is a file containg code written by somenody else(usually) which can be imported and used in our python program.
+
+Pip:
+Pip is the package manager for python. You can use pip to install a module on your system.
+
+pip install flask #Installs Flask Module
+
+Types of Modules:
+
+1. Built in Modules(Pre-installed):
+These modules are ready to import and use and ships with the python interpreter. There is no need to install such
+modules explicitly.
+Some examples of built in modules are os, random etc.
+
+2. External Modules(Need to install using pip):
+These modules are imported from a third party file or can be installed using a package manager like pip or conda. Since
+this code is written by someone else, we can install different versions of a same module with time.
+Some examples of external modules are tensorflow, flask, etc.
+'''
